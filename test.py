86af755ad7a0afdb16141e657e98dbef57d225db
GUNUPATI UDAY SATHYA MURUGAN <132286726+murugangunupati@users.noreply.github.com>
2025-03-05 18:46:11 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
+from dotenv import load_dotenv
+import os
+
+# Load API key from .env file
+load_dotenv()
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+print("API key Loaded:", GOOGLE_API_KEY) # Test if it works 
+
+ The loaded API_KEY is = "AIzaSyARIslpvh3yfTNS77SSoYxqE08Ar-RgOhQ"
+                           
 
